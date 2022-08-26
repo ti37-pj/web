b2d5f50131907f1a03ec4d25c7e72b0b7998699e
Renan da Silva Barbosa <89998760+RenanSB11@users.noreply.github.com>
2022-08-26 17:31:22 -0300

    Estrutura de pastas e inicio do menu.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Menu from './Componentes/Menu/Menu';
+
+function App() {
+  return (
+    <>
+      <h1>Lelexo Bar</h1>
+      <Menu/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Menu/Menu.js
@@ -0,0 +1,15 @@
+import { FunctionComponent } from 'react'
+import './Menu.css'
+
+const Menu:FunctionComponent = () => {
+	return (
+		<>
+          <a>Início</a> 
+          <a>Cardápio</a> 
+          <a>Contato</a>
+          <a>Localização</a>
+	    </>
+	)
+}
+
+export default Menu
