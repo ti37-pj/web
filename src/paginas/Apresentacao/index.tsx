c6c9ce79c5f73052828c98050e23131165984075
Renan da Silva Barbosa <89998760+RenanSB11@users.noreply.github.com>
2022-10-04 14:38:21 -0300

    Limita a altura do carousel

--- src/paginas/Apresentacao/index.tsx
+++ src/paginas/Apresentacao/index.tsx
@@ -5,7 +5,6 @@ import Menu from "../../componentes/Menu";
 import './styles.module.css';
 import Categoria from "../../modelos/Categoria";
 import Produto from "../../modelos/Produto";
-
 import Rodape from "../../componentes/Rodape";
 import api from "../../api";
 
@@ -38,39 +37,36 @@ function PaginaApresentacao() {
         buscaCategorias()
     }, []);
 
-    return (
-        <>
-            <header className="apresentacao">
+return (
+  <>
+    <header className="apresentacao">
+      <div>
+        <h1>Lelexo Bar</h1>
+      </div>
+      <div>
+        <Menu />
+      </div>
+    </header>
 
-                <div>
-                <h1>Lelexo Bar</h1>
-                </div>
+    <main id="inicio">
 
-                <div>
-                    <Menu /> 
-
-                </div>
-            </header>
-
-            <main id="inicio">
-
-            <Carousel>
+    <Carousel>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://images.adsttc.com/media/images/5dd4/b679/3312/fd69/2000/0044/slideshow/_RAS0103.jpg?1574221388"
-          alt="First slide"
+        className="d-block w-100"
+        src="https://images.adsttc.com/media/images/5dd4/b679/3312/fd69/2000/0044/slideshow/_RAS0103.jpg?1574221388"
+        alt="First slide"
         />
         <Carousel.Caption>
-          <h3>Chopp em dobro de quartas e domingos</h3>        </Carousel.Caption>
+          <h3>Chopp em dobro de quartas e domingos</h3>
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://images.adsttc.com/media/images/5dd4/b411/3312/fd69/2000/0039/slideshow/_RAS0070.jpg?1574220771"
-          alt="Second slide"
+        className="d-block w-100"
+        src="https://images.adsttc.com/media/images/5dd4/b411/3312/fd69/2000/0039/slideshow/_RAS0070.jpg?1574220771"
+        alt="Second slide"
         />
-
         <Carousel.Caption>
           <h3>Horário de funcionamento</h3>
           <p>Terça a Quinta 11h as 21H</p>
@@ -79,9 +75,9 @@ function PaginaApresentacao() {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
-          src="https://images.adsttc.com/media/images/5dd4/b1bb/3312/fd89/f700/0005/slideshow/_RAS0027.jpg?1574220166"
-          alt="Third slide"
+        className="d-block w-100"
+        src="https://images.adsttc.com/media/images/5dd4/b1bb/3312/fd89/f700/0005/slideshow/_RAS0027.jpg?1574220166"
+        alt="Third slide"
         />
 
         <Carousel.Caption>
@@ -89,24 +85,19 @@ function PaginaApresentacao() {
       </Carousel.Item>
     </Carousel>
 
-          <section style={estilo.containerCardapio} >
+    <section style={estilo.containerCardapio} >
 
-            <div id="cardapio" >
-              <h2>Cardápio</h2>
-              {categorias.map(categoria =>
-                  <CardapioCategoria categoria={categoria} produtos={produtosDaCategoria(categoria)} />
-              )}
-            </div>
-
-          </section>
-
-
-
-            <Rodape />
-
-      </main>
-        </>
-    );
+    <div id="cardapio" >
+      <h2>Cardápio</h2>
+      {categorias.map(categoria =>
+      <CardapioCategoria categoria={categoria} produtos={produtosDaCategoria(categoria)} />
+      )}
+    </div>
+    </section>
+    <Rodape />
+  </main>
+  </>
+);
 }
 
 const estilo = {
